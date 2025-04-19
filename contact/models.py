5e73c422681ec3fd9e44d3dea81f9567ef4a2bd3
iguin26 <igordaniel.lopes@gmail.com>
2025-04-19 16:58:08 -0300

    model contact update

--- contact/models.py
+++ contact/models.py
@@ -14,12 +14,12 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=51)
     last_name = models.CharField(max_length=51, blank=True)
     phone = models.CharField(max_length=51)
-
     email = models.EmailField(max_length=255, blank=True)
-
     created_date = models.DateTimeField(default=timezone.now)
-
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
 
+    
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
